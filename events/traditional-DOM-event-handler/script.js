43eecaa8590d2f776f49b6eede6569a38d61c2de
DonCastillo <don.qcastillo@yahoo.com>
2020-07-13 18:09:00 -0600

    updated traditional DOM event handler file

--- events/traditional-DOM-event-handler/script.js
+++ events/traditional-DOM-event-handler/script.js
@@ -1,12 +1,10 @@
 function animate()
 {
-    //alert('hello');
     var bar = document.getElementsByClassName('bar');
   
     for(var i = 0; i < bar.length; ++i){
         if( bar[i].hasAttribute('style') ) {
             bar[i].removeAttribute('style');
-            //bar[i].setAttribute( 'style', 'width:0%;' );
         } else {
             bar[i].setAttribute('style', 'width:100%;' );
         }
